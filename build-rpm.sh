b50a13afc511b5848b98a32c2e79db2ed2665659
Robert Bruce Park <rbpark@exolucere.ca>
2012-05-13 22:26:00 -0500

    Tweak build process.

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 python2.7 ./setup.py sdist
+mkdir -p rpmbuild/SOURCES
+mv dist/* rpmbuild/SOURCES
 cd rpmbuild/SPECS
 rpmbuild -ba gottengeography.spec
+cd ../..
+mv rpmbuild/RPMS/noarch/* dist/
+
