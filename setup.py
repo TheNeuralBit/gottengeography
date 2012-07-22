79faf1423b4c920904fa4b1944a73fb33dbfd757
Robert Bruce Park <rbpark@exolucere.ca>
2012-07-22 17:45:54 -0500

    Minor fixes.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ data_files = [
     ('/usr/share/icons/hicolor/scalable/apps', ['data/%s.svg' % PACKAGE]),
     ('/usr/share/glib-2.0/schemas', ['data/ca.exolucere.%s.gschema.xml' % PACKAGE]),
     ('/usr/share/applications', ['data/%s.desktop' % PACKAGE]),
-    ('share/doc/' + PACKAGE, ['README.md', 'AUTHORS', 'COPYING', 'THANKS']),
+    ('share/doc/' + PACKAGE, ['README.md', 'AUTHORS', 'THANKS']),
     ('share/' + PACKAGE, ['data/cities.txt', 'data/trackfile.ui', 'data/camera.ui',
         'data/%s.ui' % PACKAGE, 'data/%s.svg' % PACKAGE])
 ]
