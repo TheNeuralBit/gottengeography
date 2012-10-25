3d0e457d700c519885f6aace7edb5342f2b6a767
Robert Bruce Park <r@robru.ca>
2012-10-25 12:33:47 -0500

    Resurrect the sexiness of th Makefile.

--- Makefile
+++ Makefile
@@ -2,3 +2,16 @@
 
 doctest:
 	python3 -m doctest gg/*.py -v
+
+check:
+	nosetests-3.2 --with-doctest -v
+
+flakes:
+	pyflakes gottengeography setup.py gg test
+
+lint:
+	pylint --include-ids=y gg test -d \
+		E0611,E1101,E1120,W0613,W0403,W0142,W0141,W0102,R0903
+
+install:
+	python3 setup.py install
--- setup.py
+++ setup.py
@@ -15,20 +15,6 @@ from distutils.command.install import install
 from gg.version import PACKAGE, VERSION, AUTHOR, EMAIL
 
 
-TESTS = dict(
-    nose=('nosetests-3.2', '--with-doctest', '-v'),
-    flakes=('pyflakes', 'gottengeography', 'setup.py', 'gg', 'test'),
-    lint=('pylint', '--include-ids=y', 'gg', 'test', '-d',
-          'E0611,E1101,E1120,W0613,W0403,W0142,W0141,W0102,R0903'),
-)
-
-
-test = TESTS.get(argv[1])
-if test:
-    Popen(test).communicate()
-    exit()
-
-
 root = '--root' in ' '.join(argv)
 
 
