0a1c797b6162f5231a457991bd6cb7b5b9f46530
Robert Bruce Park <r@robru.ca>
2013-07-09 19:48:24 -0700

    Don't import from gg.common in setup.py.
    The indirect dependency on gsettings breaks when the settings schema
    is not already installed, which is the norm, eg, inside a pbuilder
    environment.
    
    Curse you desrt!!!

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ from distutils.command.build_py import build_py as _build_py
 from distutils.command.install import install
 
 from gg.version import PACKAGE, VERSION, AUTHOR, EMAIL
-from gg.common import ignored
 
 
 root = '--root' in ' '.join(argv)
@@ -42,7 +41,7 @@ class build_py(_build_py):
     """
     def build_module(self, module, module_file, package):
         if (module_file == 'gg/build_info.py'):
-            with ignored(KeyError):
+            try:
                 iobj = self.distribution.command_obj['install']
                 with open(module_file, 'w') as module_fp:
                     module_fp.write(build_info_template.format(
@@ -51,6 +50,8 @@ class build_py(_build_py):
                         version=(VERSION if root else Popen(('git', 'describe'),
                             stdout=PIPE).communicate()[0].strip())
                     ))
+            except KeyError:
+                pass
 
         _build_py.build_module(self, module, module_file, package)
 
