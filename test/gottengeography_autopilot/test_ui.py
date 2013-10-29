c946c599f7033df9e1b678df648fef9f48e21a83
Robert Bruce Park <r@robru.ca>
2013-10-28 22:14:43 -0700

    Successfully test the GtkEntryCompletion widget.

--- test/gottengeography_autopilot/test_ui.py
+++ test/gottengeography_autopilot/test_ui.py
@@ -1,17 +1,43 @@
 # Author: Robert Park <robru@gottengeography.ca>, (C) 2013
 # Copyright: See COPYING file included with this distribution.
 
-from autopilot.matchers import Eventually
 from gottengeography_autopilot import GottenGeographyTestCase
-from testtools.matchers import Equals, NotEquals
+from autopilot.matchers import Eventually
+from testtools.matchers import Equals
+from time import sleep
 
 
 class UITests(GottenGeographyTestCase):
-
     def setUp(self):
         super(UITests, self).setUp()
+        self.search_box = self.get_widget(
+            'GtkEntry', BuilderName='search_box')
 
-    def test_search_bar(self):
-        search_box = self.get_widget('GtkEntry', BuilderName='search_box')
-        self.pointer.click_object(search_box)
-        self.keyboard.type('Edmonton, Al')
+    def test_find_edmonton(self):
+        self.search_for('Edmo')
+        self.assertThat(
+            self.main_window.get_properties()['title'],
+            Eventually(Equals('Edmonton, Alberta, Canada')))
+
+        self.search_for('Victoria, Bri')
+        self.assertThat(
+            self.main_window.get_properties()['title'],
+            Eventually(Equals('Victoria, British Columbia, Canada')))
+
+        self.search_for('Bost')
+        self.assertThat(
+            self.main_window.get_properties()['title'],
+            Eventually(Equals('Sandbostel, Lower Saxony, Germany')))
+
+    def search_for(self, string):
+        # Triple click to select any existing text
+        for i in range(3):
+            self.pointer.click_object(self.search_box)
+        self.keyboard.type(string)
+        sleep(1)
+
+        # Click the first result in the drop-down.
+        x, y = self.pointer.position()
+        self.pointer.move(x, y+20)
+        self.pointer.click()
+        sleep(1)
