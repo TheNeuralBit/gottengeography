d85b0d5f421cf0001a0566b6981faf95fcedaecf
Robert Bruce Park <r@robru.ca>
2015-01-01 14:18:42 -0800

    100% test coverage for gg.territories.

--- tests/test_territories.py
+++ tests/test_territories.py
@@ -8,3 +8,19 @@ class TerritoriesTestCase(BaseTestCase):
 
     def setUp(self):
         super().setUp()
+
+    def test_countries(self):
+        self.assertEqual(self.mod.countries['CA'], 'Canada')
+        self.assertEqual(self.mod.get_country('MT'), 'Malta')
+        self.assertIsNone(self.mod.get_country('Narnia'))
+
+    def test_territories(self):
+        self.assertEqual(self.mod.territories['CA.02'], 'British Columbia')
+        self.assertEqual(self.mod.get_state('CA', '01'), 'Alberta')
+        self.assertEqual(self.mod.get_state('US', 'WI'), 'Wisconsin')
+        self.assertIsNone(self.mod.get_state('US', 'Fakerson'))
+
+    def test_zones(self):
+        self.assertIn('Atlantic', self.mod.zones)
+        self.assertIn('Pacific', self.mod.zones)
+        self.assertIn('America', self.mod.tz_regions)
