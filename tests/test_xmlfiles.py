952b22194b38f13a76379700513d728509c104fa
Robert Bruce Park <r@robru.ca>
2014-12-31 22:22:07 -0800

    Test track_color_changed.

--- tests/test_xmlfiles.py
+++ tests/test_xmlfiles.py
@@ -17,3 +17,16 @@ class XmlFilesTestCase(BaseTestCase):
         self.assertEqual(color, self.mod.Clutter.Color.new.return_value)
         self.mod.Clutter.Color.new.assert_called_once_with(
             127.99609375, 255.99609375, 127.99609375, 192.0)
+
+    def test_track_color_changed(self):
+        s = Mock()
+        p = [Mock(), Mock()]
+        self.mod.make_clutter_color = Mock()
+        self.mod.track_color_changed(s, p)
+        s.get_color.assert_called_once_with()
+        self.mod.Gst.set_color.assert_called_once_with(s.get_color.return_value)
+        self.mod.make_clutter_color.assert_called_once_with(s.get_color.return_value)
+        c = self.mod.make_clutter_color.return_value
+        c.lighten.return_value.lighten.assert_called_once_with()
+        p[0].set_stroke_color.assert_called_once_with(c)
+        p[1].set_stroke_color.assert_called_once_with(c.lighten().lighten())
