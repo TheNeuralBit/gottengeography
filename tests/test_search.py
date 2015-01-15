7ea81fef02070410b1a52c7d1935424003a69272
Robert Bruce Park <r@robru.ca>
2015-01-14 23:27:03 -0800

    100% test coverage for gg.search.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,7 @@
 """Test the classes and functions defined by gg/search.py"""
 
+from mock import Mock, call
+
 from tests import BaseTestCase
 
 
@@ -8,3 +10,67 @@ class SearchTestCase(BaseTestCase):
 
     def setUp(self):
         super().setUp()
+        self.mod.Widgets = Mock()
+        self.controller = self.mod.SearchController()
+
+    def test_search_init(self):
+        self.mod.Gtk.ListStore.new.assert_called_once_with([str, float, float])
+        self.mod.Gtk.EntryCompletion.new.assert_called_once_with()
+        search = self.mod.Gtk.EntryCompletion.new.return_value
+        self.assertIsNone(self.controller.search)
+        search.set_model.assert_called_once_with(self.controller.results)
+        search.connect.assert_called_once_with(
+            'match-selected', self.controller.search_completed)
+        entry = self.mod.Widgets.search_box
+        entry.set_completion.assert_called_once_with(search)
+        self.assertEqual(4, len(entry.connect.mock_calls))
+
+    def test_search_load_result(self):
+        searched = set()
+        append = Mock()
+        entry = Mock()
+        entry.get_text.return_value = 'Regina'
+        self.controller.load_results(entry, append, searched)
+        # Exact match is there
+        self.assertIn(
+            call(('Regina, Saskatchewan, Canada', 50.45008, -104.6178)),
+            append.mock_calls)
+        # Substring match is there
+        self.assertIn(
+            call(('Villa Regina, Rio Negro, Argentina', -39.1, -67.06667)),
+            append.mock_calls)
+        # Anything with 'reg' is also there
+        self.assertIn(
+            call(('Waregem, Flemish, Belgium', 50.88898, 3.42756)),
+            append.mock_calls)
+        self.assertEqual(len(append.mock_calls), 265)
+        self.assertEqual(searched, set(['reg']))
+
+    def test_search_load_result_cache(self):
+        searched = set(['reg'])
+        append = Mock()
+        entry = Mock()
+        entry.get_text.return_value = 'Regina'
+        self.controller.load_results(entry, append, searched)
+        self.assertEqual(append.mock_calls, [])
+
+    def test_search_search_completed(self):
+        entry = Mock()
+        model = Mock()
+        model.get.return_value = [1, 2]
+        itr = Mock()
+        self.controller.search_completed(entry, model, itr)
+        self.mod.MapView.emit.assert_called_once_with('realize')
+        self.mod.Widgets.redraw_interface.assert_called_once_with()
+        model.get.assert_called_once_with(
+            itr, self.mod.LATITUDE, self.mod.LONGITUDE)
+        self.mod.MapView.center_on.assert_called_once_with(1, 2)
+
+    def test_search_repeat_last_search(self):
+        entry = Mock()
+        model = Mock()
+        self.controller.last_search = 'foo'
+        self.controller.search_completed = Mock()
+        self.controller.repeat_last_search(entry, model)
+        self.controller.search_completed.assert_called_once_with(
+            entry, model, 'foo')
